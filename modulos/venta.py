8fec2750e45eabb08c30a52e93297ee464d33181
Debbs2410 <00043322@uca.edu.sv>
2025-10-31 13:47:11 -0600

    Create venta.py

---
+++ modulos/venta.py
@@ -0,0 +1,8 @@
+# modulos/venta.py
+
+import streamlit as st
+
+def mostrar_venta():
+    """Muestra un mensaje de prueba en la aplicación Streamlit."""
+    st.title("Hola Mundo")
+    st.write("¡Este es un mensaje de prueba para verificar si la app está funcionando!")
